8ff2fed1be9af601562806f81e91e3815ffdfe4d
Markopolo619 <markmadi123@protonmail.com>
2021-11-25 19:17:43 +0200

    Used Axios in code

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import NewPost from './Components/NewPost';
 import PostPage from './Components/PostPage';
+import EditPost from './Components/EditPost';
 import About from './Components/About';
 import Missing from './Components/Missing';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-import api from './api/posts'
-import { set } from 'date-fns/esm';
+import api from './api/posts';
 
 function App() {
-  const [posts, setPosts] = useState([
-  ])
+  const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const history = useHistory();
 
   useEffect(() => {
@@ -26,19 +27,20 @@ function App() {
       try {
         const response = await api.get('/posts');
         setPosts(response.data);
-      } catch (error) {
-        if(error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else{
-          console.log(`Error: ${error.message}`)
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
         }
       }
     }
 
     fetchPosts();
-  }, [])  
+  }, [])
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -47,27 +49,47 @@ function App() {
 
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
-  
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    try{
-      const allPosts = [...posts, newPost];
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
       setPosts(allPosts);
       setPostTitle('');
       setPostBody('');
       history.push('/');
     } catch (err) {
-      console.log(`Error: ${err.message}`)
+      console.log(`Error: ${err.message}`);
     }
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    history.push('/');
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      history.push('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   }
 
   return (
@@ -85,6 +107,16 @@ function App() {
             setPostTitle={setPostTitle}
             postBody={postBody}
             setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/edit/:id">
+          <EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
           />
         </Route>
         <Route path="/post/:id">
--- src/Components/PostPage.js
+++ src/Components/PostPage.js
@@ -1,31 +1,30 @@
-import { Fragment } from "react";
-import { useParams, Link} from "react-router-dom"
+import { useParams, Link } from "react-router-dom";
 
-const PostPage = ({ posts, handleDelete}) => {
+const PostPage = ({ posts, handleDelete }) => {
     const { id } = useParams();
-    const post = posts.find(post => (post.id).toString() ===id);
+    const post = posts.find(post => (post.id).toString() === id);
     return (
         <main className="PostPage">
             <article className="post">
-                {post && 
+                {post &&
                     <>
                         <h2>{post.title}</h2>
                         <p className="postDate">{post.datetime}</p>
                         <p className="postBody">{post.body}</p>
-                        <button onClick={() => handleDelete(post.id)}>
+                        <Link to={`/edit/${post.id}`}><button className="editButton">Edit Post</button></Link>
+                        <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                             Delete Post
                         </button>
                     </>
                 }
                 {!post &&
-                    
-                    <section>
-                        <h2>"Post Not Found"</h2>
-                        <p>Well that's disappointing</p>
+                    <>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing.</p>
                         <p>
                             <Link to='/'>Visit Our Homepage</Link>
                         </p>
-                    </section>
+                    </>
                 }
             </article>
         </main>
