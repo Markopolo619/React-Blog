b32bbf978ffbacbb7a6d01681ce0e6e1e200e1af
Markopolo619 <markmadi123@protonmail.com>
2021-12-03 22:04:29 +0200

    Installed react icons and added custom hooks

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,12 @@
-const Header = ({ title }) => {
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
+
+const Header = ({ title, width }) => {
     return (
         <header className="Header">
             <h1>{title}</h1>
+            {width > 768 ? <FaMobileAlt />
+                : width < 800 ? <FaTabletAlt />
+                    : <FaLaptop />}
         </header>
     )
 }
